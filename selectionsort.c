5befa13732ae3812d8a30e959028c6006db08bdb
jiangguer77 <50871622+jiangguer77@users.noreply.github.com>
2019-07-25 16:34:21 +0800

    selection    sort
    时间复杂度O（n^2）  
    总体思想：在一组数中找到最小的然后与第一个交换。不断循环。
    第一层for是大局的，在第二个for中找出最小的数，并得到最小数的位置min（本来在不断的比大小中可能位置小的为j，将其赋值给min），退出后将其放在第一个arr【0】上，将最小位置和第一个位置交换。在第一个for中不断往下走，所以arr【6】这个数组就重新组装了。

---
+++ selectionsort.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int arr[6]={12,11,35,90,10,78};
+	int i,min,j,t;
+	for(i=0;i<6;i++)
+	{	
+	min=i; //大局点
+	for(j=i+1;j<6;j++)
+		if(arr[j]<arr[min])）
+        	min=j;       //找出最小的数 值为arr[min] （j不断的改变 若小就不断地交换）
+	t=arr[i];
+	arr[i]=arr[min];
+	arr[min]=t;
+}
+for(i=0;i<6;i++)
+printf("%d\n",arr[i]); 
+}
