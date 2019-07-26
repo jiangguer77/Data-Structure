13316b4bf8a1fd5173f455befbe27753addac749
jiangguer77 <50871622+jiangguer77@users.noreply.github.com>
2019-07-26 10:47:36 +0800

    selection sort2
    再写一遍选择排序感觉这样好理解些....虽然时间复杂度没啥变化....

--- selectionsort.c
+++ selectionsort.c
@@ -6,12 +6,12 @@ int main()
 	for(i=0;i<6;i++)
 	{	
 	min=i; //大局点
-	for(j=i+1;j<6;j++)
+	for(j=min+1;j<6;j++)
 		if(arr[j]<arr[min])）
-        	min=j;       //找出最小的数 值为arr[min] （j不断的改变 若小就不断地交换）
-	t=arr[i];
-	arr[i]=arr[min];
-	arr[min]=t;
+        	//min=j;       //找出最小的数 值为arr[min] （j不断的改变 若小就不断地交换）
+	t=arr[min];
+	arr[min]=arr[j];
+	arr[j]=t;
 }
 for(i=0;i<6;i++)
 printf("%d\n",arr[i]); 
